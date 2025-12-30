fd938731e474a3620d63ffa7e8f290feb4f26fb8
Low-Frequency <94133096+Low-Frequency@users.noreply.github.com>
2025-12-30 10:45:36 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # PVE-authentication-library
-This bash library provides a simple workflow for requesting authentication tickets from a proxmox node
+
+This bash library provides a simple workflow for requesting authentication tickets from a proxmox node.
+
+It started as a simple script to authenticate the [bpg/proxmox](https://github.com/bpg/terraform-provider-proxmox) Terraform provider inside a CI/CD pipeline and quickly evolved to a full bash library. The library currently only supports user/password authentication with optional TOTP, but might be expanded with API token support and a simple API wrapper in the future.
+
+The workflow with this library is split into four steps:
+
+1. Initializing configuration
+2. Calculating TOTP
+3. Authenticating
+4. Getting the authentication ticket and CSRF prevention token
+
+Calculating TOTP is only necessary for the initial TOTP setup. If TOTP is already set up, you can pass the TOTP secret via the respective environment variable, or set it in the configuration file and the library will automatically calculate a TOTP token if needed.
+
+## Dependencies
+
+This library depends on the following commands:
+
+| Command | Provided by |
+|---------|-------------|
+| `curl` | curl |
+| `jq` | jq |
+| `xxd` | vim-common |
+| `base32` | coreutils |
+| `openssl` | openssl |
+
+## Configuration
+
+The library is configured via environment variables, or a config file. The following variables can be set:
+
+| Variable | Description | Required | Default |
+|----------|-------------|----------|---------|
+| PROXMOX_VE_INSECURE_TLS | Enables insecure communication for self signed certificates | no | 0 |
+| PROXMOX_VE_ENDPOINT | The endpoint URL for communication with the Proxmox node | yes | |
+| PROXMOX_VE_USERNAME | Username for authenticating. Has to be in the form of [username]@[realm] | yes | |
+| PROXMOX_VE_PASSWORD | Password for authenticationg | yes | |
+| PROXMOX_VE_TOTP_SECRET | TOTP secret for MFA calculation | only if TOTP is enabled for the user | |
+
+The library preferes environment variables over the config file, so if you set a config option in both the environment and the file, it will use the environment variable. A mixed configuration is also possible, for example you can configure all secrets as environment variables, but the user and endpoint remain in the config file. Defaults have the least priority and only get set if not otherwise specified.
+
+## Usage
+
+### Loading the library
+
+```bash
+source pve_auth.lib.sh
+```
+
+### Initialize configuration
+
+```bash
+pve_init [config_file] [reinit]
+```
+
+Config file is only needed if the environment variables are not set.
+
+You can set the `reinit` parameter to `1` to force a configuration change, for example if you wanted to switch endpoints, or use a different user. Omit this parameter otherwise.
+
+### TOTP calculation
+
+Only necessary for initial TOTP setup.
+
+```bash
+pve_calc_totp "${PROXMOX_VE_TOTP_SECRET}"
+```
+
+### Authentication
+
+```bash
+pve_auth
+```
+
+### Getting the authentication ticket
+
+```bash
+export PROXMOX_VE_AUTH_TICKET=$(pve_get_ticket)
+```
+
+### Getting the CSRF prevention token
+
+```bash
+export PROXMOX_VE_CSRF_PREVENTION_TOKEN=$(pve_get_csrf_token)
+```
---
+++ pve_auth.lib.sh
@@ -0,0 +1,351 @@
+#!/bin/bash
+
+### This library provides functions to request an authentication ticket from a Proxmox node
+#!  The library can be configured either through environment variables, or a config file
+#!  The following environment variables can be used for configuration:
+#!
+#!  PROXMOX_VE_ENDPOINT
+#!      Endpoint URL to curl for getting the authentication ticket. Has to include the protocol and port
+#!
+#!  PROXMOX_VE_USERNAME
+#!      Username to authenticate with in the form of [user]@[realm]
+#!
+#!  PROXMOX_VE_PASSWORD
+#!      Password for the user
+#!
+#!  PROXMOX_VE_TOTP_SECRET
+#!      Base32 encoded TOTP secret for TOTP calculation if the user has MFA configured
+#!      Can be omitted if no MFA is configured
+#!
+#!  PROXMOX_VE_INSECURE_TLS
+#!      Trigger for accepting insecure communication over TLS. Used with self signed certificates
+#!
+#!
+#!  Example config file:
+#!
+#!  PROXMOX_VE_ENDPOINT="https://pve.example.com"
+#!  PROXMOX_VE_USERNAME="root@pam"
+#!  PROXMOX_VE_PASSWORD="secret"
+#!  PROXMOX_VE_TOTP_SECRET="BASE32SECRET"
+#!  PROXMOX_VE_INSECURE_TLS=0
+#!
+#!
+#!  Public API:
+#!    pve_init [config_file] [reinit]
+#!    pve_calc_totp
+#!    pve_auth
+#!    pve_get_ticket
+#!    pve_get_csrf_token
+
+# Guard against double-sourcing
+if [[ -n "${_PROXMOX_AUTH_LIB_LOADED:-}" ]]; then
+  return 0
+fi
+
+readonly _PROXMOX_AUTH_LIB_LOADED=1
+
+# Dependencies and options
+readonly _pve_curl_opts=(--disable --silent --show-error --fail)
+readonly _pve_deps=(
+  "curl"
+  "jq"
+  "xxd"
+  "base32"
+  "openssl"
+)
+
+# Internal config
+_PVE_INITIALIZED=0
+_PVE_ENDPOINT=""
+_PVE_USERNAME=""
+_PVE_PASSWORD=""
+_PVE_TOTP_SECRET=""
+_PVE_INSECURE_TLS=0
+_PVE_AUTH_TICKET=""
+_PVE_CSRF_PREVENTION_TOKEN=""
+_PVE_AUTHENTICATED=0
+
+# Color helpers
+pve_auth_err() {
+  echo -e >&2 "\033[0;31mERROR: $*\033[0m"
+}
+
+pve_auth_ok() {
+  echo -e "\033[0;32m$*\033[0m"
+}
+
+# Check dependencies
+pve_chk_dep() {
+  for dep in "${_pve_deps[@]}"; do
+    if ! command -v "${dep}" >/dev/null; then
+      pve_auth_err "Missing dependency: ${dep}"
+      return 1
+    fi
+  done
+
+  return 0
+}
+
+# Load config file
+pve_load_config_file() {
+  local file="$1"
+
+  if [[ -z "${file}" ]]; then
+    return 0
+  fi
+
+  if ! [[ -f "${file}" ]]; then
+    pve_auth_err "Config file not found: ${file}"
+    return 1
+  fi
+
+  source "${file}"
+}
+
+# Check environment variables
+pve_load_env() {
+  if [[ -n "${PROXMOX_VE_ENDPOINT:-}" ]]; then
+    _PVE_ENDPOINT="${PROXMOX_VE_ENDPOINT%/}"
+  fi
+
+  if [[ -n "${PROXMOX_VE_USERNAME:-}" ]]; then
+    _PVE_USERNAME="${PROXMOX_VE_USERNAME}"
+  fi
+
+  if [[ -n "${PROXMOX_VE_PASSWORD:-}" ]]; then
+    _PVE_PASSWORD="${PROXMOX_VE_PASSWORD}"
+  fi
+
+  if [[ -n "${PROXMOX_VE_TOTP_SECRET:-}" ]]; then
+    _PVE_TOTP_SECRET="${PROXMOX_VE_TOTP_SECRET}"
+  fi
+
+  if [[ -n "${PROXMOX_VE_INSECURE_TLS:-}" ]]; then
+    _PVE_INSECURE_TLS="${PROXMOX_VE_INSECURE_TLS}"
+    case "${_PVE_INSECURE_TLS}" in
+      1) ;;
+      *) _PVE_INSECURE_TLS=0 ;;
+    esac
+  fi
+}
+
+# Set defaults
+pve_set_defaults() {
+  : "${_PVE_INSECURE_TLS:=0}"
+}
+
+# Initialize config
+pve_init() {
+  local config_file="${1:-}"
+  local reinit="${2:-0}"
+
+  # Add possibility to clear configuration and reinitialize afterwards
+  if [[ ${reinit} -eq 1 ]]; then
+    _PVE_INITIALIZED=0
+    _PVE_ENDPOINT=""
+    _PVE_USERNAME=""
+    _PVE_PASSWORD=""
+    _PVE_TOTP_SECRET=""
+    _PVE_INSECURE_TLS=0
+  fi
+
+  # Prevent unwanted reinitializing
+  if [[ ${_PVE_INITIALIZED} -eq 1 ]]; then
+    return 0
+  fi
+
+  pve_reset_auth
+  pve_set_defaults
+  pve_load_config_file "${config_file}"
+  pve_load_env
+
+  # Validation
+  if [[ -z "${_PVE_ENDPOINT}" ]]; then
+    pve_auth_err "Missing endpoint"
+    return 1
+  fi
+
+  if [[ -z "${_PVE_USERNAME}" ]]; then
+    pve_auth_err "Missing username"
+    return 1
+  fi
+
+  if [[ -z "${_PVE_PASSWORD}" ]]; then
+    pve_auth_err "Missing password"
+    return 1
+  fi
+
+  _PVE_INITIALIZED=1
+
+  return 0
+}
+
+# API error handling function
+pve_chk_api_err() {
+  local resp="$1"
+
+  if jq -e '.errors != null' >/dev/null <<<"${resp}"; then
+    return 1
+  fi
+
+  return 0
+}
+
+# Calculate TOTP token
+pve_calc_totp() {
+  local totp_secret="$1"
+  local count
+  local hexkey
+  local hash
+  local extracted
+  local offset
+
+  count="$(printf '%.16x' $(($(date +%s)/30)))"
+  hexkey="$(echo -n "${totp_secret}" | base32 -d | xxd -p)"
+  hash="$(
+    echo -n "${count}" |
+    xxd -r -p |
+    openssl dgst -sha1 -mac HMAC -macopt hexkey:"${hexkey}" |
+    awk '{print $NF}'
+  )"
+  offset="$((16#${hash:39}))"
+  extracted="${hash:$((offset * 2)):8}"
+
+  printf '%06d\n' "$(((16#$extracted & 16#7fffffff) % 1000000))"
+}
+
+# Failsafe to reset authentication variables on failure
+pve_reset_auth() {
+  _PVE_AUTH_TICKET=""
+  _PVE_CSRF_PREVENTION_TOKEN=""
+  _PVE_AUTHENTICATED=0
+}
+
+# Main auth function
+pve_auth() {
+  local proxmox_api_ticket_path='/api2/json/access/ticket'
+  local resp
+  local auth_ticket
+  local resp_csrf
+  local need_tfa
+  local totp
+  local curl_opts=("${_pve_curl_opts[@]}")
+
+  # Check dependencies
+  if ! pve_chk_dep; then
+    pve_reset_auth
+    return 1
+  fi
+
+  # Check for initialization
+  if [[ "${_PVE_INITIALIZED:-0}" -eq 0 ]]; then
+    pve_auth_err "Library not initialized. Please run 'pve_init' first"
+    pve_reset_auth
+    return 1
+  fi
+
+  # Set insecure TLS
+  if [[ "${_PVE_INSECURE_TLS:-0}" -eq 1 ]]; then
+    curl_opts+=(--insecure)
+  fi
+
+  resp=$(curl "${curl_opts[@]}" \
+    --data-urlencode "username=${_PVE_USERNAME}" \
+    --data-urlencode "password=${_PVE_PASSWORD}" \
+    "${_PVE_ENDPOINT}${proxmox_api_ticket_path}") || {
+      pve_reset_auth
+      return 1
+    }
+  
+  # Handle Proxmox API errors
+  if ! pve_chk_api_err "${resp}"; then
+    pve_auth_err "$(jq -r '.errors | to_entries[] | "\(.key): \(.value)"' <<<"${resp}")"
+    pve_reset_auth
+    return 1
+  fi
+
+  # Extract data from API call
+  auth_ticket=$(jq -r '.data.ticket' <<<"${resp}")
+  resp_csrf=$(jq -r '.data.CSRFPreventionToken' <<<"${resp}")
+  need_tfa=$(jq -r '.data.NeedTFA // 0' <<<"${resp}")
+
+  ## Check if the response payload needs a TFA (totp)
+  if [[ "$need_tfa" == "1" ]]; then
+    if [[ -z "${_PVE_TOTP_SECRET:-}" ]]; then
+      pve_auth_err "TOTP required, but no TOTP secret configured"
+      pve_reset_auth
+      return 1
+    else
+      totp=$(pve_calc_totp "${_PVE_TOTP_SECRET}")
+    fi
+  
+    # Authenticate with TOTP
+    resp=$(curl "${curl_opts[@]}" \
+      -H "CSRFPreventionToken: ${resp_csrf}" \
+      --data-urlencode "username=${_PVE_USERNAME}" \
+      --data-urlencode "tfa-challenge=${auth_ticket}" \
+      --data-urlencode "password=totp:${totp}" \
+      "${_PVE_ENDPOINT}${proxmox_api_ticket_path}")
+  
+    # Handle Proxmox API errors (TOTP step)
+    if ! pve_chk_api_err "${resp}"; then
+      pve_auth_err "$(jq -r '.errors | to_entries[] | "\(.key): \(.value)"' <<<"${resp}")"
+      pve_reset_auth
+      return 1
+    fi
+  
+    auth_ticket=$(jq -r '.data.ticket' <<<"${resp}")
+    resp_csrf=$(jq -r '.data.CSRFPreventionToken' <<<"${resp}")
+  fi
+
+  # Export credentials
+  if [[ -z "${auth_ticket}" ]]; then
+    pve_auth_err "Could not get auth ticket"
+    pve_reset_auth
+    return 1
+  else
+    _PVE_AUTH_TICKET="${auth_ticket}"
+    pve_auth_ok "Successfully requested auth ticket"
+  fi
+  
+  if [[ -z "${resp_csrf}" ]]; then
+    pve_auth_err "Could not get CSRF token"
+    pve_reset_auth
+    return 1
+  else
+    _PVE_CSRF_PREVENTION_TOKEN="${resp_csrf}"
+    pve_auth_ok "Successfully requested CSRF token"
+  fi
+
+  if [[ -z "${_PVE_AUTH_TICKET:-}" ]]; then
+    pve_auth_err "Error while getting authentication ticket"
+    pve_reset_auth
+    return 1
+  elif [[ -z "${_PVE_CSRF_PREVENTION_TOKEN:-}" ]]; then
+    pve_auth_err "Error while getting CSRF token"
+    pve_reset_auth
+    return 1
+  else
+    _PVE_AUTHENTICATED=1
+  fi
+
+  return 0
+}
+
+pve_get_ticket() {
+  if [[ "${_PVE_AUTHENTICATED:-0}" -eq 0 ]]; then
+    pve_auth_err "Not authenticated. Please run 'pve_auth' first"
+    return 1
+  fi
+
+  echo "${_PVE_AUTH_TICKET}"
+}
+
+pve_get_csrf_token() {
+  if [[ "${_PVE_AUTHENTICATED:-0}" -eq 0 ]]; then
+    pve_auth_err "Not authenticated. Please run 'pve_auth' first"
+    return 1
+  fi
+
+  echo "${_PVE_CSRF_PREVENTION_TOKEN}"
+}
